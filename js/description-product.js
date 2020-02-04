d80c1acf257a2ce6f592808f7fa8d2afd23ba27c
Milafrn <kaah.fernandess@hotmail.com>
2020-02-04 19:23:55 -0300

    Aula 014 - Desafio Avançado

--- js/description-product.js
+++ js/description-product.js
@@ -1,22 +1,60 @@
 const $heart = window.document.querySelector(".-heart");
 const $stars = document.querySelectorAll(".icon.-star");
-console.log($stars[0].src);
+
 $heart.addEventListener("click", handleClick);
 
 function handleClick() {
   $heart.classList.toggle("-active");
 }
 
-function changeStarImage(event) {
-  const element = event.target;
-  element.classList.toggle("-active");
-  if (element.classList.contains("-active")) {
-    element.setAttribute("src", "img/Star-active.png");
-  } else {
-    element.setAttribute("src", "img/Star.png");
-  }
+arrStars = Array.from($stars);
+
+for (let i = 0; i < arrStars.length; i++) {
+  arrStars[i].addEventListener("click", event => {
+    const element = event.target;
+    element.classList.toggle("-active");
+
+    let current = element.previousElementSibling;
+    while (!!current) {
+      element.classList.add("-active");
+      current.classList.add("-active");
+      current = current.previousElementSibling;
+    }
+
+    current = element.nextElementSibling;
+    while (!!current) {
+      element.classList.remove("-Active");
+      current.classList.remove("-active");
+      current = current.nextElementSibling;
+    }
+
+    if (element.classList.contains("-active")) {
+      reset();
+      switch (i) {
+        case 4:
+          arrStars[4].setAttribute("src", "img/Star-active.png");
+          console.log("4", arrStars[4]);
+        case 3:
+          arrStars[3].setAttribute("src", "img/Star-active.png");
+          console.log("3", arrStars[3]);
+        case 2:
+          arrStars[2].setAttribute("src", "img/Star-active.png");
+          console.log("2", arrStars[2]);
+        case 1:
+          arrStars[1].setAttribute("src", "img/Star-active.png");
+        case 0:
+          arrStars[0].setAttribute("src", "img/Star-active.png");
+          console.log("0", arrStars[0]);
+          break;
+      }
+    } else {
+      reset();
+    }
+  });
 }
 
-for (let star of $stars) {
-  star.addEventListener("click", changeStarImage);
+function reset() {
+  for (let star of $stars) {
+    star.setAttribute("src", "img/Star.png");
+  }
 }
