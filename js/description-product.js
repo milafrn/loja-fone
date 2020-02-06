e5feecea7b75855f94b7f873e5c2891b2c366aac
Milafrn <kaah.fernandess@hotmail.com>
2020-02-05 21:30:27 -0300

    Aula 015 - Ser simples é bom (Deixando o codigo igual ao da aula)

--- js/description-product.js
+++ js/description-product.js
@@ -1,60 +1,12 @@
-const $heart = window.document.querySelector(".-heart");
-const $stars = document.querySelectorAll(".icon.-star");
+const $heart = document.querySelector(".-heart");
+const $stars = document.querySelectorAll(".star");
 
 $heart.addEventListener("click", handleClick);
 
+$stars.forEach(function($star) {
+  $star.addEventListener("click", handleClick);
+});
+
 function handleClick() {
-  $heart.classList.toggle("-active");
-}
-
-arrStars = Array.from($stars);
-
-for (let i = 0; i < arrStars.length; i++) {
-  arrStars[i].addEventListener("click", event => {
-    const element = event.target;
-    element.classList.toggle("-active");
-
-    let current = element.previousElementSibling;
-    while (!!current) {
-      element.classList.add("-active");
-      current.classList.add("-active");
-      current = current.previousElementSibling;
-    }
-
-    current = element.nextElementSibling;
-    while (!!current) {
-      element.classList.remove("-Active");
-      current.classList.remove("-active");
-      current = current.nextElementSibling;
-    }
-
-    if (element.classList.contains("-active")) {
-      reset();
-      switch (i) {
-        case 4:
-          arrStars[4].setAttribute("src", "img/Star-active.png");
-          console.log("4", arrStars[4]);
-        case 3:
-          arrStars[3].setAttribute("src", "img/Star-active.png");
-          console.log("3", arrStars[3]);
-        case 2:
-          arrStars[2].setAttribute("src", "img/Star-active.png");
-          console.log("2", arrStars[2]);
-        case 1:
-          arrStars[1].setAttribute("src", "img/Star-active.png");
-        case 0:
-          arrStars[0].setAttribute("src", "img/Star-active.png");
-          console.log("0", arrStars[0]);
-          break;
-      }
-    } else {
-      reset();
-    }
-  });
-}
-
-function reset() {
-  for (let star of $stars) {
-    star.setAttribute("src", "img/Star.png");
-  }
+  this.classList.toggle("-active");
 }
