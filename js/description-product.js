eaa1bc45f13e8ad9160f455a369fc739df0114c7
Milafrn <kaah.fernandess@hotmail.com>
2020-02-06 22:47:50 -0300

    Aula 014 - Bug heart (Deixando o codigo igual ao da aula)

--- js/description-product.js
+++ js/description-product.js
@@ -1,11 +1,46 @@
 const $heart = document.querySelector(".-heart");
 const $stars = document.querySelectorAll(".star");
+const positionLast = $stars.length - 1;
 
 $heart.addEventListener("click", handleClick);
 
-$stars.forEach(function($star) {
-  $star.addEventListener("click", handleClick);
+$stars.forEach(function($star, key) {
+  if (key == 0) {
+    $star.addEventListener("click", firstStar);
+  }
+
+  if (key > 0 && key < positionLast) {
+    $star.addEventListener("click", function() {
+      middleStar(key);
+    });
+  }
+
+  if (key == positionLast) {
+    $star.addEventListener("click", lastStar);
+  }
 });
+
+function firstStar() {
+  $stars.forEach(function($star) {
+    $star.classList.remove("-active");
+  });
+  this.classList.add("-active");
+}
+
+function middleStar(index) {
+  $stars.forEach(function($star, key) {
+    $star.classList.remove("-active");
+    if (key <= index) {
+      $star.classList.add("-active");
+    }
+  });
+}
+
+function lastStar() {
+  $stars.forEach(function($star) {
+    $star.classList.add("-active");
+  });
+}
 
 function handleClick() {
   this.classList.toggle("-active");
